3a2a1855633681b6245833190dca1c452cd9a0df
Sarangthem Amarjit Meetei <sarangthemamarjit123@gmail.com>
2022-08-27 09:51:01 +0530

    Ambulance detail Complete

---
+++ lib/constant.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/cupertino.dart';
+
+const List<Map<String, dynamic>> ambulancedata = [
+  {
+    "id": 1,
+    "ambulance_name": "Ambulance 1",
+    "ambulance_type": "Freezing Ambulance",
+    "ambulance_image": "assets/images/ambulance1.jpg",
+    "ambulance_detail": {
+      "ambulance_name": "Ambulance 1",
+      "ambulance_image": "assets/images/ambulance1.jpg",
+      "ambulance_address": "Singjamei Makha Leikai",
+      "ambulance_number": "+917095145623"
+    }
+  },
+  {
+    "id": 2,
+    "ambulance_name": "Ambulance 2",
+    "ambulance_type": "Freezing Ambulance",
+    "ambulance_image": "assets/images/ambulance2.jpg",
+    "ambulance_detail": {
+      "ambulance_name": "Ambulance 2",
+      "ambulance_image": "assets/images/ambulance2.jpg",
+      "ambulance_address": "Singjamei Makha Leikai",
+      "ambulance_number": "+917095145623"
+    }
+  },
+  {
+    "id": 3,
+    "ambulance_name": "Ambulance 3",
+    "ambulance_type": "Freezing Ambulance",
+    "ambulance_image": "assets/images/ambulance3.jpg",
+    "ambulance_detail": {
+      "ambulance_name": "Ambulance 3",
+      "ambulance_image": "assets/images/ambulance3.jpg",
+      "ambulance_address": "Singjamei Makha Leikai",
+      "ambulance_number": "+917095145623"
+    }
+  },
+  {
+    "id": 4,
+    "ambulance_name": "Ambulance 4",
+    "ambulance_type": "Freezing Ambulance",
+    "ambulance_image": "assets/images/ambulance4.jpg",
+    "ambulance_detail": {
+      "ambulance_name": "Ambulance 4",
+      "ambulance_image": "assets/images/ambulance4.jpg",
+      "ambulance_address": "Singjamei Makha Leikai",
+      "ambulance_number": "+917095145623"
+    }
+  },
+  {
+    "id": 5,
+    "ambulance_name": "Ambulance 5",
+    "ambulance_type": "Freezing Ambulance",
+    "ambulance_image": "assets/images/ambulance5.jpg",
+    "ambulance_detail": {
+      "ambulance_name": "Ambulance 5",
+      "ambulance_image": "assets/images/ambulance5.jpg",
+      "ambulance_address": "Singjamei Makha Leikai",
+      "ambulance_number": "+917095145623"
+    }
+  },
+  {
+    "id": 6,
+    "ambulance_name": "Ambulance 6",
+    "ambulance_type": "Freezing Ambulance",
+    "ambulance_image": "assets/images/ambulance6.jpg",
+    "ambulance_detail": {
+      "ambulance_name": "Ambulance 6",
+      "ambulance_image": "assets/images/ambulance6.jpg",
+      "ambulance_address": "Singjamei Makha Leikai",
+      "ambulance_number": "+917095145623"
+    }
+  },
+  {
+    "id": 7,
+    "ambulance_name": "Ambulance 7",
+    "ambulance_type": "Freezing Ambulance",
+    "ambulance_image": "assets/images/ambulance7.jpg",
+    "ambulance_detail": {
+      "ambulance_name": "Ambulance 7",
+      "ambulance_image": "assets/images/ambulance7.jpg",
+      "ambulance_address": "Singjamei Makha Leikai",
+      "ambulance_number": "+917095145623"
+    }
+  },
+  {
+    "id": 8,
+    "ambulance_name": "Ambulance 8",
+    "ambulance_type": "Freezing Ambulance",
+    "ambulance_image": "assets/images/ambulance8.jpg",
+    "ambulance_detail": {
+      "ambulance_name": "Ambulance 8",
+      "ambulance_image": "assets/images/ambulance8.jpg",
+      "ambulance_address": "Singjamei Makha Leikai",
+      "ambulance_number": "+917095145623"
+    }
+  },
+  {
+    "id": 9,
+    "ambulance_name": "Ambulance 9",
+    "ambulance_type": "Freezing Ambulance",
+    "ambulance_image": "assets/images/ambulance9.png",
+    "ambulance_detail": {
+      "ambulance_name": "Ambulance 9",
+      "ambulance_image": "assets/images/ambulance9.png",
+      "ambulance_address": "Singjamei Makha Leikai",
+      "ambulance_number": "+917095145623"
+    }
+  },
+  {
+    "id": 10,
+    "ambulance_name": "Ambulance 10",
+    "ambulance_type": "Freezing Ambulance",
+    "ambulance_image": "assets/images/ambulance10.jpg",
+    "ambulance_detail": {
+      "ambulance_name": "Ambulance 10",
+      "ambulance_image": "assets/images/ambulance10.jpg",
+      "ambulance_address": "Singjamei Makha Leikai",
+      "ambulance_number": "+917095145623"
+    }
+  },
+  {
+    "id": 11,
+    "ambulance_name": "Ambulance 11",
+    "ambulance_type": "Freezing Ambulance",
+    "ambulance_image": "assets/images/ambulance11.jpg",
+    "ambulance_detail": {
+      "ambulance_name": "Ambulance 11",
+      "ambulance_image": "assets/images/ambulance11.jpg",
+      "ambulance_address": "Singjamei Makha Leikai",
+      "ambulance_number": "+917095145623"
+    }
+  },
+  {
+    "id": 12,
+    "ambulance_name": "Ambulance 12",
+    "ambulance_type": "Freezing Ambulance",
+    "ambulance_image": "assets/images/ambulance12.jpg",
+    "ambulance_detail": {
+      "ambulance_name": "Ambulance 12",
+      "ambulance_image": "assets/images/ambulance12.jpg",
+      "ambulance_address": "Singjamei Makha Leikai",
+      "ambulance_number": "+917095145623"
+    }
+  },
+  {
+    "id": 13,
+    "ambulance_name": "Ambulance 13",
+    "ambulance_type": "Freezing Ambulance",
+    "ambulance_image": "assets/images/ambulance13.jpg",
+    "ambulance_detail": {
+      "ambulance_name": "Ambulance 13",
+      "ambulance_image": "assets/images/ambulance13.jpg",
+      "ambulance_address": "Singjamei Makha Leikai",
+      "ambulance_number": "+917095145623"
+    }
+  },
+  {
+    "id": 14,
+    "ambulance_name": "Ambulance 14",
+    "ambulance_type": "Freezing Ambulance",
+    "ambulance_image": "assets/images/ambulance14.jpg",
+    "ambulance_detail": {
+      "ambulance_name": "Ambulance 14",
+      "ambulance_image": "assets/images/ambulance14.jpg",
+      "ambulance_address": "Singjamei Makha Leikai",
+      "ambulance_number": "+917095145623"
+    }
+  },
+  {
+    "id": 15,
+    "ambulance_name": "Ambulance 15",
+    "ambulance_type": "Freezing Ambulance",
+    "ambulance_image": "assets/images/ambulance15.jpg",
+    "ambulance_detail": {
+      "ambulance_name": "Ambulance 15",
+      "ambulance_image": "assets/images/ambulance15.jpg",
+      "ambulance_address": "Singjamei Makha Leikai",
+      "ambulance_number": "+917095145623"
+    }
+  },
+  {
+    "id": 16,
+    "ambulance_name": "Ambulance 16",
+    "ambulance_type": "Freezing Ambulance",
+    "ambulance_image": "assets/images/ambulance16.jpg",
+    "ambulance_detail": {
+      "ambulance_name": "Ambulance 16",
+      "ambulance_image": "assets/images/ambulance16.jpg",
+      "ambulance_address": "Singjamei Makha Leikai",
+      "ambulance_number": "+917095145623"
+    }
+  },
+];
+
+const TextStyle textStylefinal =
+    TextStyle(fontSize: 26, fontWeight: FontWeight.bold);
